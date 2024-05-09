bccd931fe6ec9f800fda73737d81288f42a7d1e2
azishio <azishio3141@gmail.com>
2024-05-09 16:19:50 +0900

    ✨ IndexedVecXsをイテレーション可能に
    いくつかの構造体名を変更

--- src/collection.rs
+++ src/collection.rs
@@ -4,20 +4,42 @@ use std::ops::Index;
 use fxhash::FxBuildHasher;
 use indexmap::IndexSet;
 
-use crate::vec_x::VecX;
+use crate::VecX;
 
-trait VexXCollection {}
+pub trait VecXCollection {}
 
-impl<T, const N: usize> VexXCollection for VecX<T, N> {}
+impl<T, const N: usize> VecXCollection for Vec<VecX<T, N>> {}
 
-pub struct IndexedVecXCollection<T, const N: usize> {
+pub struct IndexedVecXs<T, const N: usize> {
     pub values: IndexSet<VecX<T, N>, FxBuildHasher>,
     pub indices: Vec<usize>,
 }
 
-impl<T, const N: usize> VexXCollection for IndexedVecXCollection<T, N> {}
+impl<T, const N: usize> VecXCollection for IndexedVecXs<T, N> {}
 
-impl<T: PartialEq + Eq + Hash, const N: usize> IndexedVecXCollection<T, N> {
+impl<T: PartialEq + Eq + Hash, const N: usize> IndexedVecXs<T, N> {
+    pub fn new(
+        values: IndexSet<VecX<T, N>, FxBuildHasher>,
+        indices: Vec<usize>) -> Self {
+        Self {
+            values,
+            indices,
+        }
+    }
+
+    pub fn empty() -> Self {
+        Self {
+            values: IndexSet::<VecX<T, N>, FxBuildHasher>::default(),
+            indices: Vec::new(),
+        }
+    }
+    pub fn iter(&self) -> IndexedVecXIter<T, N> {
+        IndexedVecXIter {
+            collection: self,
+            current_index: 0,
+        }
+    }
+
     pub fn from_vec(vec: Vec<VecX<T, N>>) -> Self {
         let mut values = IndexSet::<VecX<T, N>, FxBuildHasher>::with_capacity_and_hasher(vec.len(), FxBuildHasher::default());
         let indices = vec.into_iter().map(|value| values.insert_full(value).0).collect();
@@ -29,7 +51,7 @@ impl<T: PartialEq + Eq + Hash, const N: usize> IndexedVecXCollection<T, N> {
     }
 }
 
-impl<T, const N: usize> Index<usize> for IndexedVecXCollection<T, N> {
+impl<T, const N: usize> Index<usize> for IndexedVecXs<T, N> {
     type Output = VecX<T, N>;
 
     fn index(&self, index: usize) -> &Self::Output {
@@ -38,4 +60,20 @@ impl<T, const N: usize> Index<usize> for IndexedVecXCollection<T, N> {
     }
 }
 
+pub struct IndexedVecXIter<'a, T, const N: usize> {
+    collection: &'a IndexedVecXs<T, N>,
+    current_index: usize,
+}
 
+impl<'a, T, const N: usize> Iterator for IndexedVecXIter<'a, T, N> {
+    type Item = &'a VecX<T, N>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current_index < self.collection.indices.len() {
+            self.current_index += 1;
+            Some(&self.collection[self.current_index])
+        } else {
+            None
+        }
+    }
+}
