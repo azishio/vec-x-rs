c0e7f6d9c409d89abd7ccb8e75a366ca9bc6b894
azishio <azishio3141@gmail.com>
2024-05-17 20:47:24 +0900

    ♻️ より可読性の高い実装に変更

--- src/vec_x.rs
+++ src/vec_x.rs
@@ -137,6 +137,7 @@ impl<T, const N: usize> VecX<T, N> {
     }
 }
 
+
 impl<T, const N: usize> Index<usize> for VecX<T, N> {
     type Output = T;
     fn index(&self, index: usize) -> &Self::Output {
@@ -150,82 +151,113 @@ impl<T, const N: usize> IndexMut<usize> for VecX<T, N> {
     }
 }
 
-impl<T: Num + Copy, const N: usize> Add<Self> for VecX<T, N> {
+impl<T, U, const N: usize> Add<VecX<U, N>> for VecX<T, N>
+    where T: Num + Copy,
+          U: Num + Copy + Into<T>
+{
     type Output = Self;
 
-    fn add(self, rhs: Self) -> Self::Output {
-        let data: [T; N] = self.data.into_iter().zip(rhs.data).map(|(a, b)| a + b).collect::<Vec<_>>().as_slice().try_into().unwrap();
+    fn add(mut self, rhs: VecX<U, N>) -> Self::Output {
+        (0..N).for_each(|i| self.data[i] = self.data[i] + rhs.data[i].into());
 
-        Self { data }
+        self
     }
 }
 
-impl<T: Num + Copy, const N: usize> Sub<Self> for VecX<T, N> {
+impl<T, U, const N: usize> Sub<VecX<U, N>> for VecX<T, N>
+    where T: Num + Copy,
+          U: Num + Copy + Into<T>
+{
     type Output = Self;
 
-    fn sub(self, rhs: Self) -> Self::Output {
-        let data: [T; N] = self.data.into_iter().zip(rhs.data).map(|(a, b)| a - b).collect::<Vec<_>>().as_slice().try_into().unwrap();
+    fn sub(mut self, rhs: VecX<U, N>) -> Self::Output {
+        (0..N).for_each(|i| self.data[i] = self.data[i] - rhs.data[i].into());
 
-        Self { data }
+        self
     }
 }
 
-impl<T: Num + Copy, const N: usize> Mul<Self> for VecX<T, N> {
+impl<T, U, const N: usize> Mul<VecX<U, N>> for VecX<T, N>
+    where T: Num + Copy,
+          U: Num + Copy + Into<T>
+{
     type Output = Self;
 
-    fn mul(self, rhs: Self) -> Self::Output {
-        let data: [T; N] = self.data.into_iter().zip(rhs.data).map(|(a, b)| a * b).collect::<Vec<_>>().as_slice().try_into().unwrap();
+    fn mul(mut self, rhs: VecX<U, N>) -> Self::Output {
+        (0..N).for_each(|i| self.data[i] = self.data[i] * rhs.data[i].into());
 
-        Self { data }
+        self
     }
 }
 
-impl<T: Num + Copy, const N: usize> Div<Self> for VecX<T, N> {
+
+impl<T, U, const N: usize> Div<VecX<U, N>> for VecX<T, N>
+    where T: Num + Copy,
+          U: Num + Copy + Into<T>
+{
     type Output = Self;
 
-    fn div(self, rhs: Self) -> Self::Output {
-        let data: [T; N] = self.data.into_iter().zip(rhs.data).map(|(a, b)| a / b).collect::<Vec<_>>().as_slice().try_into().unwrap();
+    fn div(mut self, rhs: VecX<U, N>) -> Self::Output {
+        (0..N).for_each(|i| self.data[i] = self.data[i] / rhs.data[i].into());
 
-        Self { data }
+        self
     }
 }
 
-impl<T: Num + Copy, const N: usize> Rem<Self> for VecX<T, N> {
+impl<T, U, const N: usize> Rem<VecX<U, N>> for VecX<T, N>
+    where T: Num + Copy,
+          U: Num + Copy + Into<T>
+{
     type Output = Self;
 
-    fn rem(self, rhs: Self) -> Self::Output {
-        let data: [T; N] = self.data.into_iter().zip(rhs.data).map(|(a, b)| a % b).collect::<Vec<_>>().as_slice().try_into().unwrap();
+    fn rem(mut self, rhs: VecX<U, N>) -> Self::Output {
+        (0..N).for_each(|i| self.data[i] = self.data[i] % rhs.data[i].into());
 
-        Self { data }
+        self
     }
 }
 
-impl<T: Num + AddAssign, const N: usize> AddAssign for VecX<T, N> {
-    fn add_assign(&mut self, rhs: Self) {
-        rhs.data.into_iter().enumerate().for_each(|(i, v)| self.data[i] += v);
+impl<T, U, const N: usize> AddAssign<VecX<U, N>> for VecX<T, N>
+    where T: Num + AddAssign + Copy,
+          U: Num + Copy + Into<T>
+{
+    fn add_assign(&mut self, rhs: VecX<U, N>) {
+        (0..N).for_each(|i| self.data[i] += rhs.data[i].into());
     }
 }
 
-impl<T: Num + SubAssign, const N: usize> SubAssign for VecX<T, N> {
-    fn sub_assign(&mut self, rhs: Self) {
-        rhs.data.into_iter().enumerate().for_each(|(i, v)| self.data[i] -= v);
+impl<T, U, const N: usize> SubAssign<VecX<U, N>> for VecX<T, N>
+    where T: Num + SubAssign + Copy,
+          U: Num + Copy + Into<T>
+{
+    fn sub_assign(&mut self, rhs: VecX<U, N>) {
+        (0..N).for_each(|i| self.data[i] -= rhs.data[i].into());
     }
 }
 
-impl<T: Num + MulAssign, const N: usize> MulAssign for VecX<T, N> {
-    fn mul_assign(&mut self, rhs: Self) {
-        rhs.data.into_iter().enumerate().for_each(|(i, v)| self.data[i] *= v);
+impl<T, U, const N: usize> MulAssign<VecX<U, N>> for VecX<T, N>
+    where T: Num + MulAssign + Copy,
+          U: Num + Copy + Into<T>
+{
+    fn mul_assign(&mut self, rhs: VecX<U, N>) {
+        (0..N).for_each(|i| self.data[i] *= rhs.data[i].into());
     }
 }
 
-impl<T: Num + DivAssign, const N: usize> DivAssign for VecX<T, N> {
-    fn div_assign(&mut self, rhs: Self) {
-        rhs.data.into_iter().enumerate().for_each(|(i, v)| self.data[i] /= v);
+impl<T, U, const N: usize> DivAssign<VecX<U, N>> for VecX<T, N>
+    where T: Num + DivAssign + Copy,
+          U: Num + Copy + Into<T>
+{
+    fn div_assign(&mut self, rhs: VecX<U, N>) {
+        (0..N).for_each(|i| self.data[i] /= rhs.data[i].into());
     }
 }
 
-impl<T: Num + RemAssign, const N: usize> RemAssign for VecX<T, N> {
-    fn rem_assign(&mut self, rhs: Self) {
-        rhs.data.into_iter().enumerate().for_each(|(i, v)| self.data[i] %= v);
+impl<T, U, const N: usize> RemAssign<VecX<U, N>> for VecX<T, N>
+    where T: Num + RemAssign + Copy,
+          U: Num + Copy + Into<T>
+{
+    fn rem_assign(&mut self, rhs: VecX<U, N>) {
+        (0..N).for_each(|i| self.data[i] %= rhs.data[i].into());
     }
 }
